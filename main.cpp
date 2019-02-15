2200d5313e91fc8d48642909f74139909aae4f5b
GhostCipher <47286235+GhostCipher@users.noreply.github.com>
2019-02-14 22:23:48 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main(){
+
+
+
+  return 0;
+}
+//////////////////////////////////////////
+//Menu Function                         //
+//////////////////////////////////////////
+//Displays the main menu and sub menus  //
+//////////////////////////////////////////
+//Parameters:                           //
+//                                      //
+//                                      //
+//////////////////////////////////////////
+//return Type: int                      //
+//selection-indicates the style and type//
+//////////////////////////////////////////
+int menu(){
+  int selection;
+
+
+  return selection;
+}
+//////////////////////////////////////////
+//createCitation Function               //
+//////////////////////////////////////////
+//Creates the final Citation            //
+//////////////////////////////////////////
+//Parameters:                           //
+//                                      //
+//                                      //
+//////////////////////////////////////////
+//return Type: string                   //
+//citation-the final citaion            //
+//////////////////////////////////////////
+
+//may need different functions for each style
+string createCitation(){
+  string citation;
+
+  return citation;
+}
