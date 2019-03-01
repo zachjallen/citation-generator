8443430d21645774730a63a4d6762d744f28f7c4
zachjallen <48104140+zachjallen@users.noreply.github.com>
2019-02-28 18:57:38 -0600

    Update main.cpp
    basic main menu shell

--- main.cpp
+++ main.cpp
@@ -22,8 +22,26 @@ int main(){
 //selection-indicates the style and type//
 //////////////////////////////////////////
 int menu(){
-  int selection;
+  
+//menu function
 
+  //variables for the user to enter menu choices
+ int selection;
+  //main menu with user input
+  cout << endl << "-----------------Citation Machine------------------" << endl << endl;
+  cout << "Which citation format would you like to use to cite your work?" << endl;
+  cout << "1. APA style" << endl << "2. MLA style" << endl;
+  cin >> selection;;
+  cout << endl << "---------------------------------------------------" << endl << endl;
+  //input validating loop
+  while (format != 1 && format != 2)
+  {
+    cout << "INVALID ENTRY!" << endl;
+    cout << "Which Format would you like to cite your work?" << endl;
+    cout << "1. APA style" << endl << "2. MLA style" << endl;
+    cin >> selection;;
+    cout << endl << "---------------------------------------------------" << endl << endl;
+  }
 
   return selection;
 }
